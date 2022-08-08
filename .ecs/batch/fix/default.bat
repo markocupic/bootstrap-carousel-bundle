2df26f18d5993a31aa70e9b734b00b7fe0bba0d2
Marko Cupic <m.cupic@gmx.ch>
2022-08-08 10:43:23 +0200

    Add easy coding standard 5 support

---
+++ .ecs/batch/fix/default.bat
@@ -0,0 +1,10 @@
+:: Run easy-coding-standard (ecs) via this batch file inside your IDE e.g. PhpStorm (Windows only)
+:: Install inside PhpStorm the  "Batch Script Support" plugin
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+vendor\bin\ecs check vendor/markocupic/bootstrap-carousel-bundle/src --fix --config vendor/markocupic/bootstrap-carousel-bundle/.ecs/config/default.php
+cd vendor/markocupic/bootstrap-carousel-bundle/.ecs./batch/fix
